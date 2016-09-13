c5c9face444dd988781734421ba3700606b7b91a
cesandoval <ce.sandova09@gmail.com>
2016-09-13 17:27:53 -0400

    added leaflet to layer viewer

---
+++ public/javascripts/layer_viewer.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([24.65, 46.6753], 11);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.light'
+}).addTo(map);
+
+
