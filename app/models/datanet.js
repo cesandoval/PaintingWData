e7e0e74348592be67c317885838ccd5bdc6d57a5
Abel B Tadesse <abelbezu@mit.edu>
2016-09-28 21:03:25 -0400

    forgotten files

--- app/models/datanet.js
+++ app/models/datanet.js
@@ -1,47 +1,30 @@
-var Sequelize = require('sequelize'),
-    Model = require('../models/models.js');
-
-var attributes = {
-  id: {
-    autoIncrement: true,
-    type: Sequelize.INTEGER,
-    primaryKey: true
-  },
-  // userId: {
-  //   type: Sequelize.INTEGER,
-  //   references: {
-  //     // This is a reference to another model
-  //     model: 'users',
-  //     // This is the column name of the referenced model
-  //     key: 'id',
-  //     // This declares when to check the foreign key constraint. PostgreSQL only.
-  //     deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-  //   },
-  //   allowNull: false
-  // },
-  layername: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    // unique: true,
-  },
-  layerids: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    // unique: true,
-  },
-  geometry: {
-    type: Sequelize.GEOMETRY,
-    allowNull: true
-  },
-  epsg: {
-    type: Sequelize.INTEGER,
-    allowNull: true
-  }
-}
-
-var options = {
-  freezeTableName: true
-}
-
-module.exports.attributes = attributes
-module.exports.options = options
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Datanet = sequelize.define('Datanet', {
+    layername: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // unique: true,
+    },
+    layerids: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // unique: true,
+    },
+    geometry: {
+      type: DataTypes.GEOMETRY,
+      allowNull: true
+    },
+    epsg: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        
+      }
+    }
+  });
+  return Datanet;
+};
