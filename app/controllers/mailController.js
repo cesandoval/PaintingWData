a6d6af3741b7af02e9380a1f0e27f604ccc15bdb
Abel B Tadesse <abelbezu@mit.edu>
2017-01-02 14:10:51 -0500

    Mailer

---
+++ app/controllers/mailController.js
@@ -0,0 +1,21 @@
+var nodemailer = require('nodemailer');
+
+var email, password; // Obtain the username and password somehow.
+var transporter = nodemailer.createTransport(`smtps://${email}@gmail.com:${password}@smtp.gmail.com`);
+
+
+module.exports.sendMail = function(mailOptions, callback){
+
+	transporter.sendMail(mailOptions, function(err, info){
+	    if(err){
+	    	callback(err, null);
+	        
+	    }
+	    else{
+		    callback(null, info);
+		}
+	 
+	});
+
+}
+
