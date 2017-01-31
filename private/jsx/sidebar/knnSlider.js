474d4c0297e35a683ed02bda1d73620d286e4b0d
Abel B Tadesse <abelbezu@mit.edu>
2017-01-30 21:09:35 -0800

    Knn setup

--- private/jsx/sidebar/knnSlider.js
+++ private/jsx/sidebar/knnSlider.js
@@ -7,19 +7,42 @@ class KnnSlider extends React.Component {
         super(props);
         
     }
+
     componentWillReceiveProps(nprops){
-        //nprops.mapStarted
-        // console.log(nprops);
+      // you will get the layers by doing nprops.layers
+      console.log("this is a layer: ", nprops.layers)
     }
+
+    handleSlide(){
+          console.log(document.getElementById('knnSlider').value)
+    }
+
+
+
+
     
     render() {
         return(
             <div className="knn-slider">
                 <p className="layer-label"> # Nearest Neighbors </p>
-                <input className="slider" type="range" min="0" max="10" step="1"/>
+                <input className="slider" 
+                       id = "knnSlider"
+                       type="range" 
+                       onChange = {() => this.handleSlide()}
+                       min="0" 
+                       max="10" 
+                       step="1"/>
             </div>
         );
     }
 }
 
-export default KnnSlider;
+
+
+const mapStateToProps = (state) => {
+    return {
+        layers:     state.sidebar.layers,
+    }
+}
+
+export default connect(mapStateToProps)(KnnSlider)
