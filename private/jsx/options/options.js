8f71777261dfc5054a019f896ebc72ded2e9ddda
dfoldesi <dalma@localprojects.net>
2017-01-31 20:09:29 -0500

    Minor style fixes

--- private/jsx/options/options.js
+++ private/jsx/options/options.js
@@ -10,13 +10,14 @@ export default class extends React.Component {
         this.state = {};
     }
     showData() {
-        
+
     }
     render() {
         return(
             <div className="options--react">
                 <OptionsForm/>
-                <Button id="show" className="buttons dataText btn buttonsText" onClick={this.showData}> Data </Button>
+                <Button id="show" className="buttons dataText btn buttonsText" onClick={this.showData}> Query Data </Button>
+                <Button id="show" className="buttons dataText btn buttonsText" onClick={this.showData}> Compute Data </Button>
             </div>
         );
     }
