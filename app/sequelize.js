e70d12eafaee3138dde095faf4926a659b019fdf
cauuser <cauuser@C02QP0A4FVH5.tld>
2017-02-06 17:45:56 -0500

    back to development settings

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize'),
     sequelize = new Sequelize('PaintingWithData_Riyadh', 'postgres', 'postgrespass', {
-        host: '52.87.253.4',
+        host: 'localhost',
         dialect: 'postgres'
     })
 
