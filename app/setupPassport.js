684cb87626fe9597a054bd16d7e8c73da59cf9e8
cesandoval <ce.sandoval09@gmail.com>
2017-02-06 18:01:37 -0500

    git user swap

--- app/setupPassport.js
+++ app/setupPassport.js
@@ -2,7 +2,7 @@ var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     auth = require('passport-local-authenticate'),
     Model = require('./models');
-
+//
 module.exports = function(app) {
   app.use(passport.initialize())
   app.use(passport.session())
