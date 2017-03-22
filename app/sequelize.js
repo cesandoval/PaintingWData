645427040e558bc223397e2961acdaec7fccefe5
cesandoval <ce.sandoval09@gmail.com>
2017-03-22 12:19:03 -0400

    no logs for sequelize

--- app/sequelize.js
+++ app/sequelize.js
@@ -3,6 +3,7 @@ var host = process.env === 'production' ? '54.227.111.118' : 'localhost';
 var Sequelize = require('sequelize'),
     sequelize = new Sequelize('PaintingWithData_Riyadh', 'postgres', 'postgrespass', {
         host: 'localhost',
+        logging: false,
         dialect: 'postgres'
     })
 
