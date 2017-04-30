b7fd32412951d9f7fb8c11fb475b68cb2f82ae4f
cesandoval <ce.sandoval09@gmail.com>
2017-04-30 17:50:54 -0400

    modified pcoord naming

--- private/jsx/sidebar/layer.js
+++ private/jsx/sidebar/layer.js
@@ -8,7 +8,6 @@ class Layer extends React.Component {
         this.changeVisibility = this.changeVisibility.bind(this);
         this.changeColor = this.changeColor.bind(this);
         this.handleCheckedEvent = this.handleCheckedEvent.bind(this);
-        console.log(props)
     }
     changeVisibility(e) {
         act.sideUpdateLayer(this.props.name, 'visible', e.target.checked);
