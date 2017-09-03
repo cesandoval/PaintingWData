5b36f785a86499ac817399055aaf7b818fde7f87
ooookai <ooookai@gmail.com>
2017-09-03 16:29:39 +0800

    update webpack app setting

--- app.js
+++ app.js
@@ -144,15 +144,17 @@ if (app.get('env') === 'development') {
   });
 }
 
- // production error handler
- // no stacktraces leaked to user
- app.use(function(err, req, res, next) {
-   res.status(err.status || 500);
-   res.json({
-     message: err.message,
-     error: {}
-   });
- });
+// production error handler
+// no stacktraces leaked to user
+if (app.get('env') === 'production') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: {}
+    });
+  });
+}
 
 
 module.exports = app;
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     output: {
         // path: path.resolve(__dirname, './dist'),
         // publicPath: '/dist/',
-        path: path.resolve(__dirname, './public/'),
+        path: path.resolve(__dirname, './public/javascripts/'),
         publicPath: '/javascripts/',
         filename: 'bundle.js'
     },
