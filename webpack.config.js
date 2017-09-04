65b90c22056e975680ff69c400d8e6f9855068b1
ooookai <ooookai@gmail.com>
2017-09-04 18:41:14 +0800

    cleaning up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,59 +10,22 @@ const publicPath = '/';
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
 const publicUrl = '';
 
-
-// Source: http://blog.techbridge.cc/2016/07/30/react-dev-enviroment-webpack-browserify/
-
-// Using HtmlWebpackPlugin，insert bundle <script> into html 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: `${__dirname}/app/index.html`,
-    filename: 'index.html',
-    inject: 'body',
-});
-
-
 module.exports = {
     // These are the "entry points" to our application.
     // This means they will be the "root" imports that are included in JS bundle.
     // The first two entry points enable "hot" CSS and auto-refreshes for JS.
     entry: [
-        // './app/index.js',
-        // 'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
-        // 'webpack-dev-server/client?http://localhost:3000/',
         'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
         './private/jsx/entry.js',
-        // 'webpack-dev-server/client?http://localhost:8080/',
     ],
-    // entry: './src/main.js',
     output: {
-        // path: path.resolve(__dirname, './dist'),
-        // publicPath: '/dist/',
         path: path.resolve(__dirname, './public/'),
         publicPath: '/javascripts/',
+        // This does not produce a real file. It's just the virtual path that is
+        // serve in development. This is the JS bundle
+        // containing code from all our entry points, and the Webpack runtime.
         filename: 'javascripts/bundle.js'
     },
-    /*
-    output: {
-        // Next line is not used in dev but WebpackDevServer crashes without it:
-        path: paths.appBuild,
-        // Add / [filename] / comments to generated require()s in the output.
-        pathinfo: true,
-        // This does not produce a real file. It's just the virtual path that is
-        // served by WebpackDevServer in development. This is the JS bundle
-        // containing code from all our entry points, and the Webpack runtime.
-        filename: 'static/js/bundle.js',
-        // There are also additional JS chunk files if you use code splitting.
-        chunkFilename: 'static/js/[name].chunk.js',
-        // This is the URL that app is served from. We use "/" in development.
-        publicPath: publicPath,
-
-        // // // //
-        path: `${__dirname}/dist`,
-        filename: 'index_bundle.js',
-    },
-    */
     resolve: {
         extensions: ['.js', '.json', '.jsx'],
     },
@@ -90,11 +53,10 @@ module.exports = {
                     cacheDirectory: true,
                 },
                 */
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
             },
-
-            // { test: /\.css$/, loader: "style!css" },
-            // { test: /\.less/, loader: 'style-loader!css-loader!less-loader' },
-
             // ** ADDING/UPDATING LOADERS **
             // The "file" loader handles all assets unless explicitly excluded.
             // The `exclude` list *must* be updated with every change to loader extensions.
@@ -135,9 +97,6 @@ module.exports = {
         ],
     },
     plugins: [
-        // HTMLWebpackPluginConfig,
-        // This is necessary to emit hot updates (currently CSS only):
-        
         // this provide hot-reload function
         new webpack.HotModuleReplacementPlugin(),
         // no reload page when any error (optional)
@@ -147,19 +106,11 @@ module.exports = {
     resolve: {
         alias: {}
     },
-    // devServer: {
-    //     inline: true,
-    //     port: 3000,
-    //     hot: true,
-    //     historyApiFallback: true,
-    //     noInfo: true,
-    //     contentBase: './public/',
-    // },
     performance: {
         hints: false
     },
-    devtool: '#eval-source-map',
     // Turn off performance hints during development because we don't do any
     // splitting or minification in interest of speed. These warnings become
     // cumbersome.
+    devtool: '#eval-source-map',
 };
