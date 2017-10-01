384af49d5f982f5dadecbb8848906c6f69acd8da
ooookai <ooookai@gmail.com>
2017-10-01 18:28:24 +0800

    [refactor-vpl] add reducer 'mapRemoveGeometry'
    try hide layer

--- private/jsx/store/reducers/map.js
+++ private/jsx/store/reducers/map.js
@@ -17,6 +17,14 @@ export default (state=initialMapState, action) => {
         case c.MAP_ADD_GEOMETRY:
             const newGeos = {geometries: Object.assign({}, state.geometries, {[action.name]: action.geometry})};
             return Object.assign({}, state, newGeos);
+        case c.MAP_REMOVE_GEOMETRY:
+            const geos = Object.assign({}, state.geometries)
+            const geo = geos[action.name]
+            if(geo){
+                geo.material.uniforms.show.value = 0.0
+                delete geos[action.name]
+            }
+            return Object.assign({}, state, {geometries: geos});
         case c.MAP_SET_OPTION_SHOW:
             const optionShow = {optionShow: action.option}
             return Object.assign({}, state, optionShow);
