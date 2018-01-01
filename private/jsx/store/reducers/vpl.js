7638f87bc81f076ee40ae12f08651dfa39c406ee
ooookai <ooookai@gmail.com>
2018-01-01 21:41:39 +0800

    style: vpl lint

--- private/jsx/store/reducers/vpl.js
+++ private/jsx/store/reducers/vpl.js
@@ -1,97 +1,94 @@
-import * as consts from '../consts';
+import * as consts from '../consts'
 
 const initialState = {
     nodes: [],
-    links: []
+    links: [],
     // nodes: [node1, node2, node3, node4],
     // links: [link1, link2, link3]
 }
 
-
-export default (state=initialState, action) => {
-
+export default (state = initialState, action) => {
     switch (action.type) {
         case consts.VLANG_ADD_LINK:
-            return {               
+            return {
                 nodes: [...state.nodes],
-                links: [
-                    ...state.links,
-                    action.link
-                ]    
-            };
+                links: [...state.links, action.link],
+            }
         case consts.VLANG_REMOVE_LINK:
-            
-            var newState = {               
+            var newState = {
                 nodes: [...state.nodes],
                 links: [
                     ...state.links.slice(0, action.index),
-                    ...state.links.slice(action.index+1),
-                ]    
-            };
-            return newState;
-            
-        case consts.VLANG_ADD_NODE:
-            return {               
-                    nodes: [
-                        ...state.nodes,
-                        action.node
-                    ],
-                    links: [
-                        ...state.links,
-                    ]    
-                };
-        case consts.VLANG_REMOVE_NODE: {
-            let newLinks = [] 
-            state.links.map((link) => {
-                if(!(link.sourceNode.ref === state.nodes[action.index].ref || 
-                   link.targetNode.ref === state.nodes[action.index].ref)){
-                    newLinks.push(link);
-                }
-               
-            })
-            return {               
-                    nodes: [
-                        ...state.nodes.slice(0, action.index),
-                        ...state.nodes.slice(action.index+1),
-                    ],
-                    links: newLinks
-                };
+                    ...state.links.slice(action.index + 1),
+                ],
             }
+            return newState
+
+        case consts.VLANG_ADD_NODE:
+            return {
+                nodes: [...state.nodes, action.node],
+                links: [...state.links],
+            }
+        case consts.VLANG_REMOVE_NODE: {
+            let newLinks = []
+            state.links.map(link => {
+                if (
+                    !(
+                        link.sourceNode.ref === state.nodes[action.index].ref ||
+                        link.targetNode.ref === state.nodes[action.index].ref
+                    )
+                ) {
+                    newLinks.push(link)
+                }
+            })
+            return {
+                nodes: [
+                    ...state.nodes.slice(0, action.index),
+                    ...state.nodes.slice(action.index + 1),
+                ],
+                links: newLinks,
+            }
+        }
         case consts.VLANG_UPDATE_NODE_POSITION: {
-            let newNode = state.nodes[action.index];
+            let newNode = state.nodes[action.index]
 
             newNode.translate = action.position
             newNode.name = action.props[action.index].name
             newNode.userLayerName = action.props[action.index].userLayerName
             newNode.property = action.props[action.index].property
 
-            return  {
-                 nodes: [
-                       ...state.nodes.slice(0, action.index),
-                       newNode,
-                       ...state.nodes.slice(action.index+1),
-                    ],
-                    links: [
-                        ...state.links,
-                    ]    
-            };
+            return {
+                nodes: [
+                    ...state.nodes.slice(0, action.index),
+                    newNode,
+                    ...state.nodes.slice(action.index + 1),
+                ],
+                links: [...state.links],
+            }
         }
         case consts.VLANG_ADD_LAYERS: {
             let allNodes = []
             action.layers.map((layer, index) => {
-                let nodeIndex = "node_"+parseInt(parseInt(index)+1);
-                let currNode = { ref: nodeIndex, type: consts.LAYER_NODE, position: {x:100, y:100+150*index}, translate: {x: 0, y: 0}, name:layer.name, userLayerName:layer.userLayerName, property:layer.property};
-                allNodes[index] = currNode;
+                let nodeIndex = 'node_' + parseInt(parseInt(index) + 1)
+                let currNode = {
+                    ref: nodeIndex,
+                    type: consts.LAYER_NODE,
+                    position: { x: 100, y: 100 + 150 * index },
+                    translate: { x: 0, y: 0 },
+                    name: layer.name,
+                    userLayerName: layer.userLayerName,
+                    property: layer.property,
+                }
+                allNodes[index] = currNode
             })
             return {
                 nodes: allNodes,
-                links: []
+                links: [],
             }
         }
 
         default: {
-            return state;
+            return state
         }
     }
 }
-
--- private/jsx/vprog/rVpl.js
+++ private/jsx/vprog/rVpl.js
@@ -330,10 +330,11 @@ class VPL extends React.Component {
             })
             .subscribe(observer('panVpl$'))
 
-        this.shiftKeyEvent$ = Rx.Observable.merge(
-            Rx.Observable.fromEvent(window, 'keydown'),
-            Rx.Observable.fromEvent(window, 'keyup')
-        )
+        this.shiftKeyEvent$ = Rx.Observable
+            .merge(
+                Rx.Observable.fromEvent(window, 'keydown'),
+                Rx.Observable.fromEvent(window, 'keyup')
+            )
             .filter(f => f.key == 'Shift')
             .map(m => m.type == 'keydown')
             .do(d => {
@@ -480,9 +481,7 @@ class VPL extends React.Component {
 
         if (!_.isEmpty(newValue)) {
             console.log(
-                `updated the ${value} => ${newValue} for '${attr}' of ${
-                    nodeKey
-                }`
+                `updated the ${value} => ${newValue} for '${attr}' of ${nodeKey}`
             )
 
             nodes[nodeKey].options[attr] = newValue
@@ -937,8 +936,7 @@ class VPL extends React.Component {
                     <g
                         key={`${nodeKey}_plug_input_${input}`}
                         ref={ref =>
-                            (this[`${nodeKey}_plug_input_${input}`] = ref)
-                        }
+                            (this[`${nodeKey}_plug_input_${input}`] = ref)}
                         className="plug"
                         data-node-key={nodeKey}
                         data-plug="true"
@@ -1194,9 +1192,8 @@ class VPL extends React.Component {
                         ref={ref => (this.mainSvgElement = ref)}
                         width="3000"
                         height="3000"
-                        viewBox={`${this.state.panning.x} ${
-                            this.state.panning.y
-                        } 3000 3000`}
+                        viewBox={`${this.state.panning.x} ${this.state.panning
+                            .y} 3000 3000`}
                         xmlns="http://www.w3.org/2000/svg"
                     >
                         {this.linkMarker()}
@@ -1210,9 +1207,8 @@ class VPL extends React.Component {
                         })}
 
                         <g
-                            transform={`translate(${this.state.panning.x},${
-                                this.state.panning.y
-                            })`}
+                            transform={`translate(${this.state.panning.x},${this
+                                .state.panning.y})`}
                         >
                             {// do not display temp link when its `from` and `to` is the same
                             // (this.state.tempLink.from.x == this.tempLink.tempLink.to.x && this.state.tempLink.from.y == this.tempLink.tempLink.to.y)
