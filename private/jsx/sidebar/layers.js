553c2dfb5c742d895c693e058c67c894b66c2f78
ooookai <ooookai@gmail.com>
2018-02-26 23:07:01 +0800

    fix(refactor-redux): set temp hashKey

--- private/jsx/sidebar/layers.js
+++ private/jsx/sidebar/layers.js
@@ -59,7 +59,18 @@ class Layers extends React.Component {
         axios
             .get('/datajson/all/' + datavoxelId, { options: {} })
             .then(({ data }) => {
-                const datasets = data
+                // TODO:CHECK: dataset's `layerKey` is `null`, should get the `layerKey` from database
+
+                // const datasets = data
+                let datasets = data.map(dataset => {
+                    const hashKey =
+                        (+new Date()).toString(32) +
+                        Math.floor(Math.random() * 36).toString(36)
+                    dataset.layerKey = hashKey
+
+                    return dataset
+                })
+
                 console.log({ datasets })
 
                 act.importDatasets({ datasets })
