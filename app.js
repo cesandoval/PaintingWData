47ca9b8b9949bf9f6ddbe4309e392d5a50ea3762
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2018-03-18 19:04:02 -0400

    add increase upload size (commented)

--- app.js
+++ app.js
@@ -76,6 +76,8 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json({ limit: '50mb' }))
+// app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
 app.use(cookieParser())
 app.use(
     session({
