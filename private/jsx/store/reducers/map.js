ba95c2e4f0a461a7024375f49c015bca7dc661d0
Christopher Xu <chxu@mit.edu>
2018-05-22 19:12:40 -0400

    Added comment

--- private/jsx/store/reducers/map.js
+++ private/jsx/store/reducers/map.js
@@ -14,6 +14,7 @@ const initialMapState = {
 
 export default (state = initialMapState, action) => {
     switch (action.type) {
+        // TODO: Fix the map parameters that are sent into this function from map.js
         case t.IMPORT_USERFILE: {
             /*eslint-disable*/
             debugger;
@@ -21,10 +22,6 @@ export default (state = initialMapState, action) => {
 
             let newState = {}
             newState.bbox = saveMap
-
-
-
-
             return Object.assign({}, state)
         }
 
