71f50492a3cc15eda4824fba52c7a3b8d4e49504
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2018-06-16 19:53:02 -0400

    debugging .NODE_ENV

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,4 +1,4 @@
-var host = process.env === 'production' ? 'pwd.cvyt4sv5tkgm.us-west-1.rds.amazonaws.com' : 'pwd-develop.cpynefgvbqsq.us-east-1.rds.amazonaws.com';
+var host = process.env.NODE_ENV === 'production' ? 'pwd.cvyt4sv5tkgm.us-west-1.rds.amazonaws.com' : 'pwd-develop.cpynefgvbqsq.us-east-1.rds.amazonaws.com';
 console.log(host, process.env)
 var Sequelize = require('sequelize'),
     sequelize = new Sequelize('PaintingWithData_Riyadh', 'postgres', 'postgrespass', {
