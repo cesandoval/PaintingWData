228e3bd5eef4e48bfdce55ea15af91b6800c020d
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2018-06-16 19:56:36 -0400

    debug message

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,5 +1,5 @@
 var host = process.env.NODE_ENV === 'production' ? 'pwd.cvyt4sv5tkgm.us-west-1.rds.amazonaws.com' : 'pwd-develop.cpynefgvbqsq.us-east-1.rds.amazonaws.com';
-console.log(host, process.env)
+console.log(host, process.env.NODE_ENV)
 var Sequelize = require('sequelize'),
     sequelize = new Sequelize('PaintingWithData_Riyadh', 'postgres', 'postgrespass', {
         logging: false,
