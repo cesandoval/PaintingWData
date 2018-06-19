012c22410fc1904ec323ebf7dd98cfaa279111b8
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2018-06-19 11:21:47 -0400

    comment out userFile

--- app/migrations/20180302205623-create-datauserfile.js
+++ app/migrations/20180302205623-create-datauserfile.js
@@ -1,37 +1,37 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     /*
+//       Add altering commands here.
+//       Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-    return queryInterface.createTable('Datauserfiles', {
-      map: {
-        type: Sequelize.JSON
-      },
-      options: {
-        type: Sequelize.JSON
-      },
-      UPL: {
-        type: Sequelize.JSON
-      },
-      datavoxelId: {
-        type: Sequelize.STRING//whatever the hash is. it's a string!
-      }
-    });
-  },
+//       Example:
+//       return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+//     */
+//     return queryInterface.createTable('Datauserfiles', {
+//       map: {
+//         type: Sequelize.JSON
+//       },
+//       options: {
+//         type: Sequelize.JSON
+//       },
+//       UPL: {
+//         type: Sequelize.JSON
+//       },
+//       datavoxelId: {
+//         type: Sequelize.STRING//whatever the hash is. it's a string!
+//       }
+//     });
+//   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+//   down: (queryInterface, Sequelize) => {
+//     /*
+//       Add reverting commands here.
+//       Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.dropTable('users');
-    */
-  }
-};
+//       Example:
+//       return queryInterface.dropTable('users');
+//     */
+//   }
+// };
