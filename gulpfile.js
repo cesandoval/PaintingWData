f16e677dbd75d82ad927efb7f02498f04ce09dd0
ooookai <ooookai@gmail.com>
2018-06-21 14:50:10 -0400

    feat(vueify): add vue-lint for vue file lint to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp')
 // var gutil = require('gulp-util')
 
+const eslint = require('gulp-eslint')
+
 var browserify = require('browserify')
 
 const source = require('vinyl-source-stream')
@@ -27,7 +29,33 @@ gulp.task('vueify', function(done) {
     })
 })
 
+gulp.task('vue-lint', () => {
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return (
+        gulp
+            .src(['./private/vue/*.*'])
+            // eslint() attaches the lint output to the "eslint" property
+            // of the file object so it can be used by other modules.
+            .pipe(
+                eslint({
+                    configFile: './private/vue/.eslintrc_vue.js',
+                    warnFileIgnored: true,
+                    ignorePath: '.gitignore',
+                    // fix: true,
+                })
+            )
+            // eslint.format() outputs the lint results to the console.
+            // Alternatively use eslint.formatEach() (see Docs).
+            .pipe(eslint.format())
+            // To have the process exit with an error code (1) on
+            // lint error, return the stream and pipe to failAfterError last.
+            .pipe(eslint.failAfterError())
+    )
+})
+
 gulp.task('vue:watch', function() {
+    // gulp.watch(['private/vue/*.*'], ['vue-lint', 'vueify'])
     gulp.watch(['private/vue/*.*'], ['vueify'])
 })
 
---
+++ private/vue/.eslintrc_vue.js
@@ -0,0 +1,46 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:vue/recommended', // or 'plugin:vue/base'
+  ],
+  plugins: [
+    'prettier', 'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': 'off',
+    'comma-dangle': ['warn', 'only-multiline'],
+    'no-var': 'warn',
+
+    'prettier/prettier': ['warn', {
+      'trailingComma': 'es5',
+      'singleQuote': true,
+      'semi': false,
+    }],
+
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 5,
+        multiline: {
+          max: 3,
+          allowFirstLine: true,
+        },
+      },
+    ],
+  }
+}
