cdecaa874df63120061951a7bf5412621e199c98
Leon Cheng <57leonardo@gmail.com>
2018-06-21 01:10:21 -0400

    update rastervals of all datalayers

--- manually-migrate-to-new-data-format.js
+++ manually-migrate-to-new-data-format.js
@@ -1,33 +1,50 @@
-var Model = require('./app/models')
+var Model = require('./app/models'),
+    async = require('async')
+
 
 // UPDATE RASTERVAL OF ALL DATALAYERS TO 0, 1, 2, 3, ...
-function updateDatalayersRasterval(){
+function updateDatalayersRasterval(callback){
+    console.log("inside updateDatalayersRasterval");
+
     Model.Datalayer.findAll({
 
-    }).then(function(result)) {
+    }).then(function(result) {
+        var numDatalayers = Object.keys(result).length;
+        if (numDatalayers == 0){
+            callback(null);
+        }
+        var numUpdated = 0;
+
+        var counters = {}; // Dictionary mapping Datafile to Highest rasterval for that datafile
         for (var key in result) {
             var datalayerObj = result[key];
-            var counters = {}; // dictionary mapping Datafile to Highest rasterval for that datafile
             if ( !(datalayerObj.datafileId in counters)) {
                 counters[datalayerObj.datafileId] = -1;
             }
             counters[datalayerObj.datafileId] = counters[datalayerObj.datafileId] + 1;
+            // console.log("counters: ", counters);
+            // console.log("datalayerObj.id: ", datalayerObj.id);
             
-            // do the update
-            Model.Datajson.update(
+            // Do the update for each datalayer
+            Model.Datalayer.update(
                 { rasterval: counters[datalayerObj.datafileId] },
                 { where: { id: datalayerObj.id } }
-            ).then(function(result) {
-                // console.log('after update:', result)
+            ).then(function(res) {
+                numUpdated += 1;
+                // console.log(numUpdated);
+                if (numUpdated == numDatalayers) {
+                    callback(null);        
+                }
             })
 
         }
-    }
+    });
 }
 
 
 // FIND DATALAYERS THAT ARE MISSING DATADBFS, BUILD AND SAVE MISSING DATADBFS FOR THEM
-function dbfsForDatalayers() {
+function dbfsForDatalayers(callback) {
+    console.log("inside dbfsForDatalayers");
     Model.Datalayer.findAll({
         // Include associated things that datalayer 'has'
         include: [
@@ -36,28 +53,38 @@ function dbfsForDatalayers() {
             },
         ],
     }).then(function(result) {
+        var numToUpdate = Object.keys(result).length;
+        if (numToUpdate == 0) {
+            callback(null);
+        }
+        var numUpdated = 0;
         // Iterate through all rows stored in Datalayers table
         for (var key in result) {
-            var row = result[key]
+            var row = result[key];
             // Parse out the Datadbf property
-            var dbfValue = row.dataValues.Datadbf
+            var dbfValue = row.dataValues.Datadbf;
             // If there is no Datadbf in this datalayer, then create the missing one, with appropriate properties
             if (dbfValue == null) {
-                var dataDbf = Model.Datadbf.build()
-                dataDbf.userId = row.userId
-                dataDbf.datalayerId = row.rasterval
-                dataDbf.datafileId = row.datafileId
-                dataDbf.properties = JSON.stringify(row.properties)
-                dataDbf.save()
+                var dataDbf = Model.Datadbf.build();
+                dataDbf.userId = row.userId;
+                dataDbf.datalayerId = row.rasterval;
+                dataDbf.datafileId = row.datafileId;
+                dataDbf.properties = JSON.stringify(row.properties);
+                dataDbf.save();
+            }
+            // console.log("updating Datadbfs");
+
+            numUpdated += 1;
+            if (numUpdated == numToUpdate) {
+                callback(null);
             }
         }
-        console.log('done going through datalayers')
-    })
+    });
 }
-// dbfsForDatalayers();
 
 // UPDATE RASTERPROPERTY IN DATAJSON FOR ROWS THAT ARE MISSING IT
-function rasterPropertiesForDatajsons() {
+function rasterPropertiesForDatajsons(callback) {
+    console.log('inside rasterPropertiesForDatajsons')
     // Find the datajson rows that are missing properties
     // Find the datavoxelIds that are in these rows
     var datajsonsWithoutPropertiesToDatavoxels = {}
@@ -75,14 +102,14 @@ function rasterPropertiesForDatajsons() {
                     datajsonObject.datafileId
             }
         }
-        console.log(
-            'datajsonsWithoutPropertiesToDatavoxels: ',
-            datajsonsWithoutPropertiesToDatavoxels
-        )
-        console.log(
-            'datajsonsWithoutPropertiesToDatafiles: ',
-            datajsonsWithoutPropertiesToDatafiles
-        )
+        // console.log(
+        //     'datajsonsWithoutPropertiesToDatavoxels: ',
+        //     datajsonsWithoutPropertiesToDatavoxels
+        // )
+        // console.log(
+        //     'datajsonsWithoutPropertiesToDatafiles: ',
+        //     datajsonsWithoutPropertiesToDatafiles
+        // )
 
         // Find the property that corresponds to a voxel
         var datavoxelToDatafileToProperty = {}
@@ -128,13 +155,18 @@ function rasterPropertiesForDatajsons() {
                     }
                 }
             }
-            console.log(
-                'datavoxelToDatafileToProperty: ',
-                datavoxelToDatafileToProperty
-            )
+            // console.log(
+            //     'datavoxelToDatafileToProperty: ',
+            //     datavoxelToDatafileToProperty
+            // )
 
             // Use the datavoxelToProperties object
             // Update all datajsons that are missing, with the correctly formatted new rasterProperty
+            var numToUpdate = Object.keys(datajsonsWithoutPropertiesToDatavoxels).length;
+            if (numToUpdate == 0){
+                callback(null);
+            }
+            var numUpdated = 0; 
             for (var k in datajsonsWithoutPropertiesToDatavoxels) {
                 var firstKey = datajsonsWithoutPropertiesToDatavoxels[k]
                 var secondKey = datajsonsWithoutPropertiesToDatafiles[k]
@@ -145,34 +177,34 @@ function rasterPropertiesForDatajsons() {
                     { rasterProperty: desiredProperty },
                     { where: { id: k } }
                 ).then(function(result) {
-                    console.log('after update:', result)
+                    console.log('updating datajson rasterProperty:', result)
+                    numUpdated += 1;
+                    if (numUpdated == numToUpdate) {
+                        callback(null);    
+                    }
                 })
             }
 
-            console.log('done going through datajsons')
         })
     })
 }
-// rasterPropertiesForDatajsons();
+
+function allDone(callback) {
+    console.log("done running file");
+}
+
 
 // FUNCTION THAT IS RUN WHEN THIS FILE IS RUN
 function runAll() {
-    console.log('starting manually-migrate-to-new-data-format.js');
-    var promiseOne = new Promise(function() {
-        var promiseTwo = new Promise(function() {
-            updateDatalayersRasterval();
-        })
-        promiseTwo.then(function() {
-            console.log('done updateDatalayersRasterval');
-            dbfsForDatalayers();
-            rasterPropertiesForDatajsons();    
-        })
-    })
-    promiseOne.then(function() {
-        console.log('done dbfsForDatalayers');
-        console.log('done rasterPropertiesForDatajsons');
-        console.log('done manually-migrate-to-new-data-format.js');
-    })
+
+    async.waterfall([
+        updateDatalayersRasterval,
+        dbfsForDatalayers,
+        rasterPropertiesForDatajsons,
+        allDone,
+    ], function (err, result) {
+    });    
+
 }
 
-runAll()
+runAll();
