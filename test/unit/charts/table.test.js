f7015b699d99708bbb6e496e22df2b5e1e14d44f
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2018-07-10 00:04:56 -0400

    added enzyme and jest configuration

---
+++ test/unit/charts/table.test.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-15';
+import Table from '../../../private/jsx/charts/table'
+
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup() {
+    const props = {
+      addTodo: jest.fn()
+    }
+
+    const enzymeWrapper = shallow(<Table {...props} />)
+
+    return {
+      props,
+      enzymeWrapper
+    }
+  }
+
+describe('Charts', () => {
+    describe('Table', () => {
+      it('should render self and subcomponents', () => {
+        const { enzymeWrapper } = setup()
+
+        expect(enzymeWrapper.find('div').hasClass('header')).toBe(true)
+
+        // expect(enzymeWrapper.find('h1').text()).toBe('todos')
+
+        // const todoInputProps = enzymeWrapper.find('TodoTextInput').props()
+        // expect(todoInputProps.newTodo).toBe(true)
+        // expect(todoInputProps.placeholder).toEqual('What needs to be done?')
+      })
+
+    })
+  })
