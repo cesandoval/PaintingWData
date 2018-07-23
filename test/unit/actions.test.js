2bce42ff6e95d1e0c46f376aeef22a1ba288a7ab
Christopher Xu <chxu@mit.edu>
2018-07-22 23:27:06 -0400

    More

--- test/unit/actions.test.js
+++ test/unit/actions.test.js
@@ -1,23 +1,24 @@
 import * as Act from '../../private/jsx/store/actions'
 import * as t from '../../private/jsx/store/types'
+import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import fetchMock from 'fetch-mock'
+// import fetchMock from 'fetch-mock'
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
 
 describe("reducer actions", () => {
-    afterEach(() => {
-        fetchMock.reset()
-        fetchMock.restore()
-    })
+    // afterEach(() => {
+    //     fetchMock.reset()
+    //     fetchMock.restore()
+    // })
 
     it("should return the corresponding reducer given the action creator", () => {
         const expectedActions = [ 
-            { type: t.IMPORT_DATASETS }
+            { type: t.IMPORT_USERFILE }
         ]
         const store = mockStore({ todos: [] })
-        return store.dispatch(Act.importDatasets()).then(() => {
+        return store.dispatch(Act.importUserfile()).then(() => {
             expect(store.getActions()).toEqual(expectedActions);
         })
     })
