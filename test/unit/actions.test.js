b3696e5ed0186a46ffee035da27f071762b65170
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2018-07-22 22:30:11 -0400

    created action unit test template

--- test/unit/actions.test.js
+++ test/unit/actions.test.js
@@ -1,24 +1,27 @@
 import * as Act from '../../private/jsx/store/actions'
 import * as t from '../../private/jsx/store/types'
-import thunk from 'redux-thunk'
-import fetchMock from 'fetch-mock'
-
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
 
 describe("reducer actions", () => {
-    afterEach(() => {
-        fetchMock.reset()
-        fetchMock.restore()
+    it("should create an action to add a dataset", () => {
+        const payload = {}
+        const expectedAction = { 
+            type: t.IMPORT_DATASETS, 
+            ...payload 
+        }
+        
+        expect(Act.importDatasets(payload)).toEqual(expectedAction);
+
     })
 
-    it("should return the corresponding reducer given the action creator", () => {
-        const expectedActions = [ 
-            { type: t.IMPORT_DATASETS }
-        ]
-        const store = mockStore({ todos: [] })
-        return store.dispatch(Act.importDatasets()).then(() => {
-            expect(store.getActions()).toEqual(expectedActions);
-        })
+    it("should create an action to load table data", () => {
+        const payload = {value:{'asthma NY DBF_Population': 22990, 'asthma NY DBF_Rate': 29.600000381469727}}
+        const expectedAction = { 
+            type: t.LOAD_TABLE_DATA, 
+            ...payload 
+        }
+        console.log(Act.loadTableData(payload))
+        
+        expect(Act.loadTableData(payload)).toEqual(expectedAction);
+
     })
 })
