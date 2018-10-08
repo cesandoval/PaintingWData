4871e13f2daffaae7c7bb3da13d121994849ae5c
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2018-10-08 01:40:58 -0400

    adding support for JSON

--- worker/fileProcessor.js
+++ worker/fileProcessor.js
@@ -69,6 +69,53 @@ function startRasterVoxelWorker(datalayerIds, req, callback){
     });
 };
 
+function pushJSON(req, callback) {
+    var datafileId = req.body.datafileId
+    var geomType = null
+    var layer = {
+        name: req.body.layername, 
+        filePath: req.body.targetPath, 
+        datafileId: req.body.datafileId
+    }
+    async.waterfall([
+        // queryRepeatedLayer(file, layer, epsg, fields, req, geomType, callback),
+        async.apply(fileViewerHelper.queryRepeatedLayer, null, layer, 4326, null, req, geomType),
+        fileViewerHelper.saveJSON,
+        fileViewerHelper.getCentroidBbox,
+        // function(file, thingsArray, callback){
+        //     //   fs_extra.remove(file, err => {
+        //     //       if (err) {
+        //     //         console.log("Error cleaning local directory: ", file);
+        //     //         console.log(err, err.stack);
+        //     //         callback(err);
+        //     //       }
+        //     //       else{
+        //     //          callback(null);
+        //     //       }
+        //     // })
+        //     // console.log(file)
+        //     // console.log(thingsArray)
+        //     callback(null, []);         
+        // },
+        // pushDataRaster
+    ], function (err, result) {
+        // Send Layer complete user email
+        Model.User.findById(req.user.id).then(function(user){
+            // Wenzhe - this is the callback for vue
+            // This will be the callback to vue.
+            // res.send({completed: true})
+            console.log('sending email! ------')
+            mailer.sendLayerEmail(user.email ,req.user.id);
+        },
+        function(err){
+            console.log(err)
+        }
+        ).then(function(){
+            callback({uploaded: true});
+        });       
+    });
+}
+
 function startShapeWorker(req, callback) {
     var id = req.user.id;
     var newEpsg = req.body.epsg;
@@ -634,3 +681,4 @@ function pushDatajson(dataJSONs, objProps, req, rowsCols, allIndices, ptDistance
 
 module.exports.processDatalayer = startRasterVoxelWorker;
 module.exports.pushShapes = startShapeWorker;
+module.exports.pushJSON = pushJSON
