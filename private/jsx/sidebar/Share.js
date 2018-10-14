08daee78f0e44557535151a10747460b34d47900
Unknown <ooookai@gmail.com>
2018-10-14 19:14:44 -0400

    feat(snapshot): draft snapshot UI and make link-copy function

--- private/jsx/sidebar/Share.js
+++ private/jsx/sidebar/Share.js
@@ -22,13 +22,29 @@ class Controls extends React.Component {
     componentWillReceiveProps() {}
 
     copyEmbedLink = () => {
-        const copyText = document.querySelector('#project-embeddable-link')
-        copyText.select()
+        const inputDom = document.querySelector('#project-embeddable-link')
+        this.execCopy(inputDom)
+    }
+
+    copySnapshotLink = snapshotLink => {
+        const inputDom = document.querySelector('#snapshot-link')
+        inputDom.value = snapshotLink
+        this.execCopy(inputDom)
+    }
+
+    execCopy = inputDom => {
+        inputDom.select()
         document.execCommand('copy')
-        message.success('Embeddable Link is Copied')
+        message.success('Link is Copied')
     }
 
     render() {
+        // /* Testing snapshot UI */
+        const snapshotImage =
+            'https://s3.amazonaws.com/data-voxel-images/18.jpg'
+
+        const snapshotLink = 'https://s3.amazonaws.com/data-voxel-images/18.jpg'
+
         return (
             <div id="sidebar-share">
                 <p>Embed Link</p>
@@ -47,6 +63,32 @@ class Controls extends React.Component {
                         addonAfter={<i className="fas fa-link" />}
                     />
                 </div>
+                <p>Snapshots</p>
+
+                {/* invisible input DOM only for copy function */}
+                <input id="snapshot-link" value="snapshotLink" readOnly />
+
+                <div
+                    className="snapshot"
+                    style={{
+                        backgroundImage: `url('${snapshotImage}')`,
+                    }}
+                >
+                    <span
+                        className="share-link"
+                        onClick={() => this.copySnapshotLink(snapshotLink)}
+                    >
+                        <i className="fas fa-link" />
+                    </span>
+                    <div className="desc">
+                        <span className="name">Case ABCDEFG</span>
+                        <span className="time">2018 Aug 20</span>
+                    </div>
+                </div>
+
+                <div className="snapshot add">
+                    <i className="fas fa-plus fa-3x" />
+                </div>
 
                 <style jsx>{`
                     #sidebar-share {
@@ -58,6 +100,73 @@ class Controls extends React.Component {
                             margin-top: 10px;
                             margin-bottom: 5px;
                             font-size: 16px;
+                        }
+
+                        #snapshot-link {
+                            opacity: 0;
+                            position: fixed;
+                            user-select: none;
+                        }
+
+                        .snapshot {
+                            $sh: 140px;
+                            margin: 10px 10px;
+                            width: 260px;
+                            height: $sh + 2px;
+
+                            $r: 5px;
+                            border: 1px solid #ccc;
+                            border-radius: $r;
+                            cursor: pointer;
+
+                            background-size: cover;
+                            background-position: center center;
+
+                            --share-link-opacity: 0;
+                            &:hover {
+                                --share-link-opacity: 1;
+                            }
+
+                            .share-link {
+                                position: absolute;
+                                right: 0;
+                                margin-right: 20px;
+                                margin-top: 6px;
+                                opacity: var(--share-link-opacity);
+                                transition: 0.5s opacity ease-out;
+                            }
+
+                            .desc {
+                                $h: 34px;
+                                bottom: 0px;
+                                background: #ffffff8c;
+                                height: $h;
+                                margin-top: $sh - $h;
+                                border-radius: 0px 0px $r $r;
+                                border: 0px solid #ccc;
+                                border-top-width: 1px;
+
+                                span {
+                                    line-height: $h;
+                                }
+
+                                .name {
+                                    float: left;
+                                    margin-left: $r;
+                                }
+
+                                .time {
+                                    float: right;
+                                    margin-right: $r;
+                                }
+                            }
+
+                            &.add {
+                                text-align: center;
+                                i {
+                                    line-height: 140px;
+                                }
+                            }
                         }
                     }
                 `}</style>
