25ad45806a641f5aac4d3dabb8338c78c0de7062
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2018-10-31 16:47:23 -0400

    added error handling and responses

--- app/controllers/sharingController.js
+++ app/controllers/sharingController.js
@@ -25,15 +25,21 @@ module.exports.uploadSnapshot = function(req, res) {
       where: {hash: hash }, 
       }).then(function(datasnapshot) {
           if (datasnapshot === null) {
-              s3Lib.uploadBlobToBucket({buf: buf}, hash, {bucket: bucket}, function(imageLink) {
-                  var newDatasnapshot = Datasnapshot.build();
-                  newDatasnapshot.datavoxelId = datavoxelId
-                  newDatasnapshot.image =  'https://s3.amazonaws.com/' + bucket + '/' + imageLink
-                  newDatasnapshot.hash = hash
-                  newDatasnapshot.name = name
-                  newDatasnapshot.save().then(function(){
-                      console.log('Datasnapshot has been created', imageLink)
-                  });   
+              s3Lib.uploadBlobToBucket({buf: buf}, hash, {bucket: bucket}, function(imageLink, err) {
+                  if (err) {
+                    console.log(err)
+                    res.json({success:false, err: err})
+                  } else {
+                    var newDatasnapshot = Datasnapshot.build();
+                    newDatasnapshot.datavoxelId = datavoxelId
+                    newDatasnapshot.image =  'https://s3.amazonaws.com/' + bucket + '/' + imageLink
+                    newDatasnapshot.hash = hash
+                    newDatasnapshot.name = name
+                    newDatasnapshot.save().then(function(){
+                        console.log('Datasnapshot has been created', imageLink)
+                        res.json({success:true})
+                    }); 
+                  }  
               });
           } else {
               console.log('Datasnapshot already exists')
