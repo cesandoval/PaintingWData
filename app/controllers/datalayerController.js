6d2a6a3880c2463a5409a451fc1af1c6b4558539
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2019-03-15 14:11:26 -0400

    migration for datafiles

--- app/controllers/datalayerController.js
+++ app/controllers/datalayerController.js
@@ -207,32 +207,11 @@ module.exports.getDatasets = function(req, res) {
             userId : req.user.id,
             deleted: {$not: true}
         },
-        include: [{
-            model: Model.Datalayer,
-            limit: 1}]
         }).then(function(datafiles){
-
             res.json({id: req.params.id, datafiles : datafiles, userSignedIn: req.isAuthenticated(), user: req.user, });
         });
 }
 
-
-/* TODO(CreateProject)
-module.exports.showDatasets = function(req, res) {
-    Model.Datafile.findAll({
-        where : {
-            userId : req.user.id,
-            deleted: {$not: true}
-        },
-        include: [{
-            model: Model.Datalayer,
-            limit: 1}]
-        }).then(function(datafiles){
-
-            res.render('createProject', {id: req.params.id, datafiles : datafiles, userSignedIn: req.isAuthenticated(), user: req.user, layerAlert: req.flash('layerAlert')[0]});
-        });
-}
-*/
 
 /**
  * Handles displaying all voxels that
